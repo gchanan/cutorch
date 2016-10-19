3fd9b948e42810cc8dc75fd3f505dfc8637e322c
Gregory Chanan <gchanan@fb.com>
2016-10-19 14:10:56 -0700

    v5

--- lib/THC/generic/THCDeviceTensorUtils.cu
+++ lib/THC/generic/THCDeviceTensorUtils.cu
@@ -21,25 +21,35 @@ toDeviceTensor(THCState* state, THCTensor* t) {
   return toDeviceTensor<T, Dim, int, DefaultPtrTraits>(state, t);
 }
 
-/*template <typename T, int Dim,
+template <typename T, int Dim,
           typename IndexT, template <typename U> class PtrTraits>
-//THC_API
 THCDeviceTensor<T, Dim, IndexT, PtrTraits>
-toDeviceTensor(THCState* state, THCudaTensor* t);
+toDeviceTensor(THCState* state, THCTensor* t) {
+  if (Dim != THCTensor_(nDimension)(state, t)) {
+    THError("THCudaTensor dimension mismatch");
+  }
+  // Determine the maximum offset into the tensor achievable; `IndexT`
+  // must be smaller than this type in order to use it.
+  ptrdiff_t maxOffset = 0;
+  IndexT sizes[Dim];
+  IndexT strides[Dim];
 
-template <typename T, int Dim, typename IndexT>
-//THC_API
-THCDeviceTensor<T, Dim, IndexT, DefaultPtrTraits>
-toDeviceTensor(THCState* state, THCudaTensor* t) {
-  return toDeviceTensor<T, Dim, IndexT, DefaultPtrTraits>(state, t);
+  for (int i = 0; i < Dim; ++i) {
+    long size = THCTensor_(size)(state, t, i);
+    long stride = THCTensor_(stride)(state, t, i);
+
+    maxOffset += (size - 1) * stride;
+
+    sizes[i] = (IndexT) size;
+    strides[i] = (IndexT) stride;
+  }
+
+  if (maxOffset > std::numeric_limits<IndexT>::max()) {
+    THError("THCudaTensor sizes too large for THCDeviceTensor conversion");
+  }
+
+  return THCDeviceTensor<T, Dim, IndexT, PtrTraits>(
+    THCTensor_(data)(state, t), sizes, strides);
 }
-
-template <typename T, int Dim>
-THCDeviceTensor<T, Dim, int, DefaultPtrTraits>
-toDeviceTensor(THCState* state, THCudaTensor* t) {
-  return toDeviceTensor<T, Dim, int, DefaultPtrTraits>(state, t);
-}*/
-
-#include "THCDeviceTensorUtils-inl.cuh"
 
 #endif
